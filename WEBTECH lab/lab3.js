12371e8c3375d601fbc1f670da94850a27a26d6d
Sam-Wadmare <159400424+Sam-Wadmare@users.noreply.github.com>
2025-02-05 15:29:25 +0530

    Add files via upload

---
+++ WEBTECH lab/lab3.js
@@ -0,0 +1,16 @@
+
+    // function myFun() {
+    //     document.getElementById(
+    //         "gfg").innerHTML = "GeeksforGeeks";
+    // }
+    function newevent() {
+        alert("Mouse move over");
+    } 
+
+    function myFunction() {
+        document.getElementById("changeit").style.color = '#3A3960';
+    }
+    function ChangeColor() {
+        document.body.style.backgroundColor='#F8E7F6';
+    }
+
