3bc2a7ab080619b595c280d0f7384fc5472e5e01
Sam-Wadmare <159400424+Sam-Wadmare@users.noreply.github.com>
2025-05-12 03:29:38 -0400

    Add files via upload

---
+++ EXTERNAL/math.js
@@ -0,0 +1,16 @@
+function add( a, b) {
+    return a + b;
+}
+function subtract( a, b) {
+    return a - b;
+}   
+function multi(a,b) {
+    return a*b;
+}
+function divide(a,b) {
+    return a/b;
+}
+ const PI = 3.14;
+    const E = 2.71;
+
+module.exports = {add, subtract, multi, divide, PI, E};
---
+++ EXTERNAL/module.js
@@ -0,0 +1,8 @@
+const {add, subtract, multi, divide,PI, E} = require("./modules/math.js");
+console.log(add(2,3));
+console.log(subtract(2,3));
+console.log(multi(2,3));
+console.log(divide(6,3));
+console.log(PI);
+console.log(E);
+console.log("program ended!!");
